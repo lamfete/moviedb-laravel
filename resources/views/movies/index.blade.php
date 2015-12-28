47e97f76b9cea34001544eac8884910625ab2f03
lamfete <lamfete@yahoo.com>
2015-12-28 16:53:49 +0700

    modify index page

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -11,11 +11,9 @@
     <tr class="bg-info">
       <th>ID</th>
       <th>Title</th>
-      <th>Rating</th>
-      <th>Cast</th>
+      <th>IMDB Rating</th>
       <th>Release</th>
       <th>Genre</th>
-      <th>Thumbnails</th>
       <th colspan="3">Actions</th>
     </tr>
   </thead>
@@ -25,10 +23,8 @@
         <td>{{ $movie->id }}</td>
         <td>{{ $movie->title }}</td>
         <td>{{ $movie->rating }}</td>
-        <td>{{ $movie->cast }}</td>
-        <td>{{ $movie->release }}</td>
+        <td>{{ date('d-m-Y', strtotime($movie->release)) }}</td>
         <td>{{ $movie->genre }}</td>
-        <td><img src="{{ asset('img/' . $movie->image . '.jpg') }}" height="35" width="30"></td>
         <td><a href="{{url('movies', $movie->id)}}" class="btn btn-primary">Detail</a></td>
         <td><a href="{{route('movies.edit', $movie->id)}}" class="btn btn-warning">Update</a></td>
         <td>
